5aa94403bba910ea6bc02bd75f0b6423e5b43da8
DESKTOP-ANBPKDU\Micho Reyes <micho.reyes@hoolisoftware.com>
2023-10-04 20:05:01 +0800

    first commit

---
+++ learnings/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.scss'
+import { Button } from 'react-bootstrap'
+
+
+const App = () => {
+  
+  return (
+    <>
+      <div className="d-flex justify-content-center">
+      <Button className="qwe">qwe</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
